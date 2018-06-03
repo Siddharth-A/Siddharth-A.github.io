5b150529f442d54de42524a797551af40239165f
Siddharth-A <34801638+Siddharth-A@users.noreply.github.com>
2018-06-03 00:58:58 -0400

    Version2.0

---
+++ js/easypiechart.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#595a5b",
+    lineWidth: 10,
+    
+    barColor: '#ffc80f',
+    trackColor:	"#595a5b",
+    size: 160,
+    animate: 500
+  });
+});
+
+
+// "mailto:siddharthahluwalia11@gmail.com?Subject=Hello there!%20&Body=Hi%20Siddharth,%20I%20was%20going%20through%20your%20website%20and%20would%20like%20to%20get%20in%20touch%20with%20you%20about"
