0297ed2e2da9e27f79feb2110f0a6fd25ce03ed0
System Administrator <root@MacBook-Pro.local>
2025-05-19 16:24:52 -0400

    Update portfolio banner, work experience, skills & achievements

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,12 +1,17 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    loop: true
+    // loop: true
+    delay: 50
 });
 
-typewriter.typeString('SOFTWARE DEVELOPER.')
-    .pauseFor(500)
-    .deleteAll()
-    .typeString('ELECTRICAL ENGINEER.')
-    .pauseFor(500)
+typewriter.typeString('Hi!')
+    // .pauseFor(10)
+    .typeString("<br> My name is Sid and I am an experienced Software Engineer with a strong background in ")
+    .typeString("<br> ASIC validation, hardware abstraction library development and GPU feature verification. ")
+    .typeString("<br> <br> Currently, I am employed @ AMD as a Staff Software Engineer where I develop apps &")
+    .typeString("<br> System Level Tests in C++ to verify ASIC functionality and performance while leading ")
+    .typeString("<br> a team of 10+ engineers.")
+    .typeString("<br> <br> I graduated from Toronto Metropoliton University (formerly Ryerson University) in 2019")
+    .typeString("<br> with a bachelors in Electrical & Computer Engineering.")
     .start();
